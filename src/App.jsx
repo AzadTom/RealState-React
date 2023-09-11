0fc4dc9fb2f216d8803c221e6206de57cbbedb70
azadtom <kumarazad2917@gmail.com>
2023-09-11 09:21:30 +0530

    react-starter-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,9 @@
-import { Route,Routes } from "react-router-dom";
-import Home from "./home";
-import Printedmug from "./components/printedmug";
-import Pillow from './components/pillow';
-import Tshirt from './components/tshirt';
-import Wedding from './components/weding';
-import Frame from './components/frame';
-import Idcard from './components/idcard';
-import Cover from './components/mobilecover';
-function App() {
+import React from 'react'
 
- 
-  
-  return(
-   <>
-   <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/printedmug" element={<Printedmug/>}/>
-      <Route path="/idcard" element={<Idcard/>}/>
-      <Route path="/tshirt" element={<Tshirt/>}/>
-      <Route path="/wedding" element={<Wedding/>}/>
-      <Route path="/frame" element={<Frame/>}/>
-      <Route path="/pillow" element={<Pillow/>}/>
-      <Route path="/cover" element={<Cover/>}/>
-   </Routes>
-    
-   </>
-  );
+function App() {
+  return (
+    <div>Welcome</div>
+  )
 }
 
 export default App
