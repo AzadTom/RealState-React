5494550b4be42173e57145786c323be4ad05cd81
azadtom <kumarazad2917@gmail.com>
2023-10-07 20:50:58 +0530

    fix1

--- src/authentication/Success.jsx
+++ src/authentication/Success.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Success() {
   return (
-    <div>Success</div>
+    <div className='flex justify-center items-center h-screen bg-[#2E2E2E]'>
+         
+    <button>Success</button>
+</div>
   )
 }
 
