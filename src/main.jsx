146d3ee413caca1151e4cb6cc13d5596067821c8
azadtom <kumarazad2917@gmail.com>
2023-10-07 17:27:20 +0530

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
-import AuthContextProvider from './contexts/authcontext.jsx'
+import { AuthContextProvider } from './contexts/AuthContextProvider.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
