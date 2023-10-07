0b8250c58fd18dcf15d33077253e1d7ede079f04
azadtom <kumarazad2917@gmail.com>
2023-10-07 20:37:00 +0530

    fix bug

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Loader() {
   return (
-    <div>Loader</div>
+    <div className='flex justify-center items-center h-screen bg-[#2E2E2E]'>
+      <button type="button" className="bg-[#1E1E1E] text-white" disabled>
+  <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+  </svg>
+  Processing...
+</button>
+    </div>
   )
 }
 
